6e11da6f650e3aebd08dba91a3937cfc927660f9
Syniah Peterson <122937585+syniahpeterson@users.noreply.github.com>
2024-08-06 16:21:36 +0000

    Created script.js
    - Added multi-text functionality

---
+++ script.js
@@ -0,0 +1,8 @@
+// index.html (I am a)
+const typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "UI/UX Designer", "Software Engineering Major", "UNC Charlotte Senior"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
